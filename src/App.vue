a856a73781129756d8605a78f4ab633f78d2427d
Tumen Bairov <bairovt@gmail.com>
2017-12-19 18:29:28 +0900

    Layout

---
+++ src/App.vue
@@ -0,0 +1,85 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      app temporary
+      fixed
+      clipped
+      v-model="drawer"
+      enable-resize-watcher
+    >
+      <v-list>
+        <v-list-tile
+          value="true"
+          v-for="(item, i) in items"
+          :key="i"
+        >
+          <v-list-tile-action>
+            <v-icon v-html="item.icon"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-navigation-drawer
+      app right
+      fixed
+      clipped
+      v-model="rightDrawer"
+    >
+      <v-layout column align-center>
+        <br>
+        <img src="/static/v.png" alt="Vuetify.js" class="mb-5" />
+        <blockquote>
+          &#8220;First, solve the problem. Then, write the code.&#8221;
+          <footer>
+            <small>
+              <em>&mdash;John Johnson</em>
+            </small>
+          </footer>
+        </blockquote>
+      </v-layout>
+    </v-navigation-drawer>
+
+    <v-toolbar fixed app
+      clipped-right
+      clipped-left
+    >
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
+        <v-icon>menu</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    <v-content>
+      <v-container fluid>
+        <router-view>
+        </router-view>
+      </v-container>
+    </v-content>
+
+    <v-footer app>
+      <span>&copy; 2017</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        drawer: false,
+        items: [{
+          icon: 'bubble_chart',
+          title: 'Inspire'
+        }],
+        rightDrawer: false,
+        title: 'ВРоду'
+      }
+    }
+  }
+</script>
