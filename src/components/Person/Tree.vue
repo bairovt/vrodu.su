326ff923842626d672bf58681b0334c9a4a44d97
Tumen Bairov <bairovt@gmail.com>
2018-03-24 08:54:42 +0900

    vis options moved to separate file

--- src/components/Person/Tree.vue
+++ src/components/Person/Tree.vue
@@ -36,63 +36,9 @@
 import axiosInst from '@/utils/axios-instance'
 import {predokRelation, potomokRelation, surName, keyFromId} from '@/filters'
 import router from '@/router'
-
-// provide the data in the vis format
-const menColor = '#2b7ce9'
-const womenColor = '#aa00ff'
-let visOptions = {
-  locale: 'ru',
-  layout: {
-    // improvedLayout: false,
-    hierarchical: {
-      // enabled: false, // def true if any attribute enable
-      // levelSeparation: 200, // def 150
-      sortMethod: "directed", //"hubsize"
-      // parentCentralization: false, // def true
-      // edgeMinimization: false, // def true
-      // blockShifting: false, // def true
-      // nodeSpacing: 200 // def 100
-    }
-  },
-  physics: {
-    // enabled: false, // def true. Быстрый рендеринг если false
-  },
-  nodes: {
-    shape: 'icon',
-    brokenImage: '/static/upload/image.jpeg',
-    font: {
-      strokeWidth: 7
-    },
-    borderWidth: 0
-    // borderWidthSelected: 5
-  },
-  edges: {
-    smooth: true,
-    arrows: {to : true }
-  },
-  groups: {
-    0: { //women
-      icon: {face: 'FontAwesome', code: '\uf007', size: 50, color: womenColor},
-      color: {border: womenColor}  // arrow color
-    },
-	  1: { //men
-      // shape: 'icon',
-      icon: {face: 'FontAwesome', code: '\uf007', size: 50, color: menColor}
-	  }
-  },
-	interaction: {
-    hover:true,
-    keyboard: {
-      enabled: false, // если включено появляется голубая рамка
-      bindToWindow: false // если true не работают кнопки - = х ъ
-    },
-    selectConnectedEdges: false,
-    tooltipDelay: 100 // 300 ms default
-  }
-};
+import {visOptions, menColor, womenColor} from '@/options/vis-opts'
 
 let network;
-// let temp = 'temp'
 
 export default {
   name: 'Tree',
@@ -109,6 +55,7 @@ export default {
     person () {return this.$store.state.person},
     loading () {return this.$store.state.loading},
     visData: function(){
+      // provide the data in the vis format
       const treeData = {nodes: [], edges: []};
       if (this.person === null || this.predki === null || this.potomki === null) return treeData;
 
